c570edfe364c5a637197f78a515e49f03c14adbb
An Nguyen <ntan.hcmiu@gmail.com>
2022-09-06 07:18:48 -0500

    adding lab 2

---
+++ Lab2/Lab2_RunMe.R
@@ -0,0 +1,15 @@
+## STAT 341: Lab #2 Instructions
+
+# place your cursor at the beginning of each line and hit the desired keys to "Run" the script:
+# Mac:     [command]+[return]
+# Windows: [Crtl]+[Enter]
+
+# you should have already installed the shiny package last week, but if you did not
+# uncomment the line below by deleting the hashtag ('#') and run it
+#install.packages("shiny")
+library(shiny)
+
+# before you run this line, make sure you change your working directory:
+# From the menu bar, choose Session > Set Working Directory > Choose Working Directory ... 
+# then navigate to the directory/folder on your computer where the extracted/uncompressed lab folder is saved
+runApp("Lab2_shiny")

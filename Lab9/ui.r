7b932a58941eee02853537c3a8ca21d035561f4c
An <atn@iastate.edu>
2022-11-11 08:00:05 -0600

    finishing lab9

--- Lab9/ui.r
+++ Lab9/ui.r
@@ -7,13 +7,19 @@ fluidPage(
     tabsetPanel(type="tabs",
 
         tabPanel(
-            h3("Activity 1 - Simplified Dice"),
+            h4("Activity 1 - Simplified Dice"),
 
             sidebarLayout(
                 sidebarPanel(
                     numericInput(
-                        inputId = 'trialsNum',
-                        label = "Number of trials (Number of times to roll this dice)",
+                        inputId = 'seed',
+                        label = "Seed",
+                        value = 1
+                    ),
+
+                    numericInput(
+                        inputId = 'size',
+                        label = "Number of times to roll this dice",
                         value = 100
                     ),
 
@@ -22,16 +28,92 @@ fluidPage(
                         label = "Roll"
                     ),
 
+                    numericInput(
+                        inputId = "trialsNum",
+                        label = "Number of trials to run",
+                        value = 20
+                    ),
 
+                    actionButton(
+                        inputId = 'sim',
+                        label = "Simulate"
+                    )                
                 ),
                 mainPanel(
-                    h4("Results of the first 30 rolls"),
-
+                    verbatimTextOutput('print1'),
                     tableOutput("table1"),
 
-                    plotOutput("plot1")
+                    plotOutput("plot1"),
+                    verbatimTextOutput('print2'),
+                    tableOutput('table2'),
+                    verbatimTextOutput('print4'),
+                    verbatimTextOutput('print5')
                 )
             )
+        ),
+
+        tabPanel(
+            h4("Activity 2: Mandel's Peas"),
+            
+            sidebarLayout(
+                sidebarPanel(
+                    numericInput(
+                        inputId="pea1",
+                        label="Yellow and round peas",
+                        value = 1,
+                        min = 1
+                    ),
+
+                    numericInput(
+                        inputId="pea2",
+                        label="Yellow and wrinkled peas",
+                        value = 1,
+                        min = 1
+                    ),
+
+                    numericInput(
+                        inputId="pea3",
+                        label="Green and round peas",
+                        value = 1,
+                        min = 1
+                    ),
+
+                    numericInput(
+                        inputId="pea4",
+                        label="Green and wrinkled peas",
+                        value = 1,
+                        min = 1
+                    ),
+
+                    actionButton('calcPeas', "Calculate probability")
+                ),
+                mainPanel(
+                    verbatimTextOutput('print6')
+                )
+            )
+        ),
+
+        tabPanel(
+            h4("Activity 3 - Realistic Dice"),
+
+            sidebarLayout(
+                sidebarPanel(
+                    radioButtons(
+                        inputId='weights',
+                        label="Choice of weight parameters",
+                        choices = c(
+                            "a1 = a2 =... = a10" = 'equal',
+                            "a1 = 1, a2 = 2, a3 = 3,..." = 'sequential',
+                            "a1 = 1, a2 = 2, a3 = 4,..., a10 = 512" = 'expo'
+                        ),
+                        selected='equal'
+                    )
+                ),
+                mainPanel(
+                    verbatimTextOutput("print7")
+                )
+            )
+
         )
     )
 )
